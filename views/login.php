fd0ef96a342d397626edb99c9f869aab0dbafcf9
Erikson Oliveira <eriksonmagno6@hotmail.com>
2018-02-20 00:34:19 -0300

    improve login page style

--- views/login.php
+++ views/login.php
@@ -1,19 +1,23 @@
 <div class="login-container">
+  <div class="login-header">
+    <h1>Monitoramento</h1>
+  </div>
   <div class="login-form">
-    <h1>Log In</h1>
+    <div class="login-icon"><i class="fa fa-user-circle" aria-hidden="true"></i></div>
 
     <?PHP echo $alert; ?>
 
     <form method="POST">
       <div class="form-group">
         <label for="email">Email:</label>
-        <input type="email" name="email" id="email" class="form-control"/>
+        <input type="email" name="email" id="email" placeholder="Email" class="form-control"/>
       </div>
       <div class="form-group">
         <label for="senha">Senha:</label>
-        <input type="password" name="senha" id="senha" class="form-control"/>
+        <input type="password" name="senha" id="senha" placeholder="Email" class="form-control"/>
       </div>
-      <input type="submit" value="Fazer Login" class="btn btn-primary"/>
+      <input type="submit" value="Fazer Login" class="btn btn-primary login-submit"/>
+      <div class="form-group login-forgot"><a href="#">Esqueci a senha</a></div>
     </form>
   </div>
 </div>
